fcb96bf4f6a838505264500e036e2470ba2b080a
MernaNashaat <mernanashaat27@gmail.com>
2020-03-07 11:08:47 +0200

    regestration component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,23 +5,31 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule} from '@angular/forms';
 
 import {
   MatCardModule,
   MatMenuModule,
-  MatButtonModule
+  MatButtonModule,
+  MatFormFieldModule,
+  MatInputModule
 } from '@angular/material';
 
 import { CardsComponent } from './cards/cards.component';
 import { DropdownComponent } from './dropdown/dropdown.component';
 import { ProfileComponent } from './profile/profile.component';
+import { RegestrationComponent } from './components/regestration/regestration.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardsComponent,
     DropdownComponent,
-    ProfileComponent
+    ProfileComponent,
+    RegestrationComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
@@ -29,7 +37,10 @@ import { ProfileComponent } from './profile/profile.component';
     BrowserAnimationsModule,
     MatCardModule,
     MatMenuModule,
-    MatButtonModule
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
