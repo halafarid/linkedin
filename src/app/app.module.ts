70e581e9ae25411222a20d23005db93c08be4454
Sara Tarek <saraagamy972@gmail.com>
2020-03-09 12:51:03 +0200

    module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { ReactiveFormsModule } from "@angular/forms";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {
   MatCardModule,
@@ -17,30 +17,30 @@ import {
   MatFormFieldModule,
   MatInputModule,
   MatCheckboxModule,
-  MatTabGroup
-} from "@angular/material";
+  // MatTabGroup,
+  // MatTab
+} from '@angular/material';
 
-import { NetworkComponent } from "./network/network.component";
+import { NetworkComponent } from './network/network.component';
 
-import { CardsComponent } from "./cards/home-card/cards.component";
-import { DropdownComponent } from "./dropdown/dropdown.component";
-import { WelcomeCardComponent } from "./cards/welcome-card/welcome-card.component";
-import { StartPostComponent } from "./cards/start-post/start-post.component";
-import { CreatePostComponent } from "./cards/create-post/create-post.component";
-import { ProfileComponent } from "./profile/profile.component";
-import { FormsComponent } from "./forms/forms.component";
-import { PostCardComponent } from "./cards/post-card/post-card.component";
-import { OptionDropdownComponent } from "./option-dropdown/option-dropdown.component";
+import { CardsComponent } from './cards/home-card/cards.component';
+import { DropdownComponent } from './dropdown/dropdown.component';
+import { WelcomeCardComponent } from './cards/welcome-card/welcome-card.component';
+import { StartPostComponent } from './cards/start-post/start-post.component';
+import { CreatePostComponent } from './cards/create-post/create-post.component';
+import { ProfileComponent } from './profile/profile.component';
+import { FormsComponent } from './forms/forms.component';
+import { PostCardComponent } from './cards/post-card/post-card.component';
+import { OptionDropdownComponent } from './option-dropdown/option-dropdown.component';
 
-import { FooterComponent } from "./footer/footer.component";
+import { FooterComponent } from './footer/footer.component';
 
-import { RegestrationComponent } from "./components/regestration/regestration.component";
-import { SigninComponent } from "./components/signin/signin.component";
-import { from } from "rxjs";
-import { CommentComponent } from "./comment/comment.component";
-import { SkillsFormComponent } from "./forms/skills-form/skills-form.component";
-import { EducationFormComponent } from "./forms/education-form/education-form.component";
-import { ExperienceFormComponent } from "./forms/experience-form/experience-form.component";
+import { RegestrationComponent } from './components/regestration/regestration.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { CommentComponent } from './comment/comment.component';
+import { SkillsFormComponent } from './forms/skills-form/skills-form.component';
+import { EducationFormComponent } from './forms/education-form/education-form.component';
+import { ExperienceFormComponent } from './forms/experience-form/experience-form.component';
 
 @NgModule({
   declarations: [
@@ -53,7 +53,6 @@ import { ExperienceFormComponent } from "./forms/experience-form/experience-form
     WelcomeCardComponent,
     StartPostComponent,
     CreatePostComponent,
-    ProfileComponent,
     FormsComponent,
     FooterComponent,
 
@@ -62,7 +61,6 @@ import { ExperienceFormComponent } from "./forms/experience-form/experience-form
 
     PostCardComponent,
     OptionDropdownComponent,
-    ProfileComponent,
     CommentComponent,
 
     SkillsFormComponent,
@@ -82,16 +80,9 @@ import { ExperienceFormComponent } from "./forms/experience-form/experience-form
     MatIconModule,
     MatFormFieldModule,
     MatInputModule,
-    ReactiveFormsModule,
-    MatListModule,
     MatCheckboxModule,
-    MatListModule,
-    MatFormFieldModule,
-    MatSlideToggleModule,
-    MatIconModule,
-    MatInputModule,
-    MatTabGroup,
-    MatCheckboxModule
+    // MatTabGroup,
+    // MatTab,
   ],
   providers: [],
   bootstrap: [AppComponent]
