8628c0b196d2b880100740d0ddfc4d54a4a25bb2
Sara Tarek <saraagamy972@gmail.com>
2020-03-14 11:42:50 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,30 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { ProfileComponent } from './Components/profile/profile.component';
 import { AddSkillsFormComponent } from '../app/Components/forms/add-skills-form/add-skills-form.component';
+import { EducationFormComponent } from '../app/Components/forms/education-form/education-form.component';
+import { ExperienceFormComponent } from '../app/Components/forms/experience-form/experience-form.component';
+import { OpportunitiesComponent } from '../app/Components/forms/opportunities/opportunities.component';
+import { ErrorPageComponent } from './Core/error-page/error-page.component';
+import { HomeComponent } from './Core/home/home.component';
+import { SigninComponent } from './Components/Account/signin/signin.component';
+import { RegestrationComponent } from './Components/Account/regestration/regestration.component';
+import { NetworkComponent } from './Components/network/network.component';
+import { SkillsFormComponent } from './Components/forms/skills-form/skills-form.component';
+import { CreatePostComponent } from './Components/cards/create-post/create-post.component';
 
 const routes: Routes = [
   { path: 'profile', component: ProfileComponent },
-  { path: 'profile/skills/add', component: AddSkillsFormComponent }
+  { path: 'skills/addnew', component: AddSkillsFormComponent },
+  { path: 'education/add', component: EducationFormComponent },
+  { path: 'experience/add', component: ExperienceFormComponent },
+  { path: 'opportunities/add', component: OpportunitiesComponent },
+  { path: 'skills/add', component: SkillsFormComponent },
+    {path: 'post/add', component: CreatePostComponent},
+  { path: 'home', component: HomeComponent },
+  { path: 'signin', component: SigninComponent },
+  { path: 'regestration', component: RegestrationComponent },
+  { path: 'network', component: NetworkComponent }
+  // { path: '**', component: ErrorPageComponent },
 ];
 
 @NgModule({
