661aa05cc56d54752f0de6f9b8080ce93e20e9ad
mariam <mariam.amen30@gmail.com>
2020-03-15 23:32:11 +0200

    managing search results

--- src/app/Core/navbar/navbar.component.ts
+++ src/app/Core/navbar/navbar.component.ts
@@ -1,3 +1,5 @@
+import { User } from "./../../Models/User";
+import { UserService } from "./../../services/user.service";
 import { Component, OnInit } from "@angular/core";
 
 @Component({
@@ -6,13 +8,31 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./navbar.component.scss"]
 })
 export class NavbarComponent implements OnInit {
-  constructor() {}
+  constructor(private userService: UserService) {}
+  users;
   subNav = false;
+  searchResult = false;
+  match = false;
   enableSeachBy() {
     this.subNav = true;
   }
   disableSeachBy() {
     this.subNav = false;
+  }
+  enableSeachResult() {
+    this.searchResult = true;
+    this.subNav = false;
+  }
+  disableSeachResult() {
+    this.searchResult = false;
+  }
+  SeachResult(e) {
+    this.enableSeachResult();
+    this.users = this.userService.getByName(e.target["value"]);
+    if (this.users.length === 0) {
+      this.match = true;
+      this.disableSeachBy();
+    }
   }
   ngOnInit() {}
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,41 +1,44 @@
-import { NgModule, Component } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { SearchComponent } from "./components/search/search.component";
+import { NgModule, Component } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-import { ProfileComponent } from './Components/profile/profile.component';
-import { AddSkillsFormComponent } from '../app/Components/forms/add-skills-form/add-skills-form.component';
-import { EducationFormComponent } from '../app/Components/forms/education-form/education-form.component';
-import { ExperienceFormComponent } from '../app/Components/forms/experience-form/experience-form.component';
-import { OpportunitiesComponent } from '../app/Components/forms/opportunities/opportunities.component';
-import { ErrorPageComponent } from './Core/error-page/error-page.component';
-import { HomeComponent } from './Core/home/home.component';
-import { SigninComponent } from './Components/Account/signin/signin.component';
-import { RegestrationComponent } from './Components/Account/regestration/regestration.component';
-import { NetworkComponent } from './Components/network/network.component';
-import { SkillsFormComponent } from './Components/forms/skills-form/skills-form.component';
-import { CreatePostComponent } from './Components/cards/create-post/create-post.component';
-import { ConnectionsComponent } from './components/network/connections/connections.component';
+import { ProfileComponent } from "./Components/profile/profile.component";
+import { AddSkillsFormComponent } from "../app/Components/forms/add-skills-form/add-skills-form.component";
+import { EducationFormComponent } from "../app/Components/forms/education-form/education-form.component";
+import { ExperienceFormComponent } from "../app/Components/forms/experience-form/experience-form.component";
+import { OpportunitiesComponent } from "../app/Components/forms/opportunities/opportunities.component";
+import { ErrorPageComponent } from "./Core/error-page/error-page.component";
+import { HomeComponent } from "./Core/home/home.component";
+import { SigninComponent } from "./Components/Account/signin/signin.component";
+import { RegestrationComponent } from "./Components/Account/regestration/regestration.component";
+import { NetworkComponent } from "./Components/network/network.component";
+import { SkillsFormComponent } from "./Components/forms/skills-form/skills-form.component";
+import { CreatePostComponent } from "./Components/cards/create-post/create-post.component";
+import { ConnectionsComponent } from "./components/network/connections/connections.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: '/signin', pathMatch: 'full'},
-  { path: 'profile', component: ProfileComponent },
-  { path: 'skills/addnew', component: AddSkillsFormComponent },
-  { path: 'education/add', component: EducationFormComponent },
-  { path: 'education/edit', component: EducationFormComponent },
-  { path: 'experience/add', component: ExperienceFormComponent },
-  { path: 'experience/edit', component: ExperienceFormComponent },
-  { path: 'opportunities/add', component: OpportunitiesComponent },
-  { path: 'skills/add', component: SkillsFormComponent },
-  { path: 'post/add', component: CreatePostComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'signin', component: SigninComponent },
-  { path: 'regestration', component: RegestrationComponent },
-  { path: 'network', component: NetworkComponent },
-  { path: 'network/connections', component: ConnectionsComponent },
-  { path: '**', component: ErrorPageComponent },
+  { path: "", redirectTo: "/signin", pathMatch: "full" },
+  { path: "profile", component: ProfileComponent },
+  { path: "skills/addnew", component: AddSkillsFormComponent },
+  { path: "education/add", component: EducationFormComponent },
+  { path: "education/edit", component: EducationFormComponent },
+  { path: "experience/add", component: ExperienceFormComponent },
+  { path: "experience/edit", component: ExperienceFormComponent },
+  { path: "opportunities/add", component: OpportunitiesComponent },
+  { path: "skills/add", component: SkillsFormComponent },
+  { path: "post/add", component: CreatePostComponent },
+  { path: "home", component: HomeComponent },
+  { path: "signin", component: SigninComponent },
+  { path: "regestration", component: RegestrationComponent },
+  { path: "network", component: NetworkComponent },
+  { path: "network/connections", component: ConnectionsComponent },
+  { path: "search/people", component: SearchComponent },
+  { path: "search/jobs", component: SearchComponent },
+  { path: "**", component: ErrorPageComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
