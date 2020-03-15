7e5b30883ffc4ba503aadd846d0c76fa14338512
mariam <mariam.amen30@gmail.com>
2020-03-15 19:06:43 +0200

    adding research options

--- src/app/Core/navbar/navbar.component.ts
+++ src/app/Core/navbar/navbar.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.scss"]
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {}
+  subNav = false;
+  enableSeachBy() {
+    this.subNav = true;
   }
-
+  disableSeachBy() {
+    this.subNav = false;
+  }
+  ngOnInit() {}
 }
