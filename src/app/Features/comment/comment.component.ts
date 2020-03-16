4569febd5f429f6caad84f2faf8285fa15bf6f7a
halafarid <hala.farid179@gmail.com>
2020-03-16 18:12:40 +0200

    Show Posts

--- src/app/Features/comment/comment.component.ts
+++ src/app/Features/comment/comment.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import {Input} from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
 @Component({
   selector: 'app-comment',
   templateUrl: './comment.component.html',
   styleUrls: ['./comment.component.scss']
 })
 export class CommentComponent implements OnInit {
-  @Input() comment:false
-  constructor() { }
+  // @Input() comment: false;
+
+  @Input() currentPost;
+  comments = [];
+
+  constructor(public userService: UserService) { }
 
   ngOnInit() {
+    this.comments = this.currentPost.comments;
   }
 
+  addComment(e) {
+    const value = e.target.value;
+    this.comments.push({commentId: 1, userId: this.currentPost.userId, body: value, likes: 0, replies: 0});
+  }
 }
--- src/app/Models/Posts.ts
+++ src/app/Models/Posts.ts
@@ -1,16 +1,15 @@
 import { UserComment } from "./UserComment";
+import { User } from 'src/app/Models/User';
 
 
 export interface Posts
 {
-    id ?:number,
-    // userId?:number,
+    id ?: number;
+    userId?: number;
     body ? :string,
-    likes?:number,
-    ImageUrls?:string[],
+    likes?:number;
+    replies?: number;
+    imageUrls?:string[],
     comments?:UserComment[]
-
-
-
      
 }
