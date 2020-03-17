d0c0300e639f7da29e3e35adf77bd0184e16b235
halafarid <hala.farid179@gmail.com>
2020-03-17 03:11:59 +0200

    add education functions

--- src/app/Components/forms/education-form/education-form.component.ts
+++ src/app/Components/forms/education-form/education-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import {Router} from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
 @Component({
   selector: 'app-education-form',
   templateUrl: './education-form.component.html',
@@ -8,20 +9,45 @@ import {Router} from '@angular/router';
 })
 export class EducationFormComponent implements OnInit {
   @Input() isShow = true;
-  editroute=this.router.url==='/education/edit';
-  myForm = new FormGroup({
+  isAdd = this.router.url === '/education/add';
+
+  currentUser = this.userService.currentUser;
+
+  educationForm = new FormGroup({
     schoolName: new FormControl('', [
       Validators.required,
       Validators.minLength(3)
     ]),
+    degree: new FormControl(),
+    FieldOfStudy: new FormControl(),
+    startYear: new FormControl(),
+    endYear: new FormControl(),
+    grade: new FormControl(),
+    activities: new FormControl(),
+    description: new FormControl()
   });
 
-  constructor(public router:Router) { }
-  
+  constructor(
+    public router: Router,
+    public userService: UserService
+  ) { }
+
   ngOnInit() {
+    if (this.isAdd) {
+      console.log('add')
+    } else {
+      console.log('edit');
+    }
+  }
+
+  submit() {
+    if (this.isAdd && this.educationForm.valid) {
+      this.userService.addEducation(this.educationForm.value);
+      this.router.navigate(['/profile']);
+    }
   }
 
   get schoolName() {
-    return this.myForm.get('schoolName');
+    return this.educationForm.get('schoolName');
   }
 }
