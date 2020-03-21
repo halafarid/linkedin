9ea3710647b45c5071c8b31f9c3fde1f06563242
mariam <mariam.amen30@gmail.com>
2020-03-21 23:57:22 +0200

    about form adding and editing

--- src/app/Components/forms/about-form/about-form.component.ts
+++ src/app/Components/forms/about-form/about-form.component.ts
@@ -1,21 +1,29 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { UserService } from "src/app/services/user.service";
+import { Component, OnInit, Input } from "@angular/core";
+import { FormGroup, FormControl } from "@angular/forms";
 
 @Component({
-  selector: 'app-about-form',
-  templateUrl: './about-form.component.html',
-  styleUrls: ['./about-form.component.scss']
+  selector: "app-about-form",
+  templateUrl: "./about-form.component.html",
+  styleUrls: ["./about-form.component.scss"]
 })
 export class AboutFormComponent implements OnInit {
+  constructor(private userService: UserService) {}
+
   @Input() isShow = true;
-
-  aboutForm = new FormGroup({
-    summary: new FormControl()
-  });
-
-  constructor() { }
+  aboutForm: FormGroup;
+  summary = new FormControl(this.userService.currentUser.About);
 
   ngOnInit() {
+    this.aboutForm = new FormGroup({
+      summary: this.summary
+    });
   }
-
+  submitSummary(summary) {
+    let index = this.userService.Users.findIndex(
+      user => user.id === this.userService.currentUser.id
+    );
+    this.userService.currentUser.About = summary;
+    this.userService.Users[index].About = summary;
+  }
 }
--- src/app/Features/message-user/message-user.component.ts
+++ src/app/Features/message-user/message-user.component.ts
@@ -1,30 +1,23 @@
-import { Component, OnInit ,TemplateRef} from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import {BsModalService ,BsModalRef} from "ngx-bootstrap/modal"
+import { Component, OnInit, TemplateRef } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { BsModalService, BsModalRef } from "ngx-bootstrap/modal";
 
 @Component({
-  selector: 'app-message-user',
-  templateUrl: './message-user.component.html',
-  styleUrls: ['./message-user.component.scss']
+  selector: "app-message-user",
+  templateUrl: "./message-user.component.html",
+  styleUrls: ["./message-user.component.scss"]
 })
 export class MessageUserComponent implements OnInit {
-
-  modalRef : BsModalRef;
-  isOpened:false;
+  modalRef: BsModalRef;
+  isOpened: false;
   myForm = new FormGroup({
-    message : new FormControl("" , Validators.required)
+    message: new FormControl("", Validators.required)
   });
-  constructor(private modalService :BsModalService) { }
+  constructor(private modalService: BsModalService) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+  public openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
   }
-  public openModal(template:TemplateRef<any>)
-  {
-    this.modalRef=this.modalService.show(template);
-  }
-  sendMessage()
-  {
-
-  }
-
+  sendMessage() {}
 }
