10c5aa732ecf58205f1401d937b97fc71a60cd87
mariam <mariam.amen30@gmail.com>
2020-03-23 23:05:59 +0200

    skills edit form

--- src/app/Components/forms/skills-form/skills-form.component.ts
+++ src/app/Components/forms/skills-form/skills-form.component.ts
@@ -1,27 +1,37 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { UserService } from "./../../../services/user.service";
+import { Component, OnInit, Input } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-skills-form',
-  templateUrl: './skills-form.component.html',
-  styleUrls: ['./skills-form.component.scss']
+  selector: "app-skills-form",
+  templateUrl: "./skills-form.component.html",
+  styleUrls: ["./skills-form.component.scss"]
 })
 export class SkillsFormComponent implements OnInit {
   @Input() isShow = true;
-
+  skills = this.userService.currentUser.skills;
   myForm = new FormGroup({
-    schoolName: new FormControl('', [
+    schoolName: new FormControl("", [
       Validators.required,
       Validators.minLength(3)
-    ]),
+    ])
   });
 
-  constructor() { }
+  constructor(private userService: UserService) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   get schoolName() {
-    return this.myForm.get('schoolName');
+    return this.myForm.get("schoolName");
+  }
+  deleteSkill(skillId) {
+    this.skills = this.skills.filter(skill => skill.id !== skillId);
+    console.log(this.skills);
+  }
+  onSave() {
+    this.userService.currentUser.skills = this.skills;
+  }
+  onCancel() {
+    this.skills = this.userService.currentUser.skills;
   }
 }
